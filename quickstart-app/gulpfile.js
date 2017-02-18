9b966f1b39681a9ce70a13e3306f0de02a83560b
Christopher Hooper <hooper.christopher@gmail.com>
2017-02-18 14:42:44 -0800

    feat(): Add file upload api server

---
+++ quickstart-app/gulpfile.js
@@ -0,0 +1,24 @@
+/**
+ * Created by chooper on 2/16/17.
+ */
+var gulp   = require( 'gulp' ),
+    server = require( 'gulp-develop-server' )
+    jshint = require('gulp-jshint');
+
+gulp.task('lint', function() {
+  return gulp.src('app.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+    // run server
+gulp.task( 'server:start', function() {
+    server.listen( { path: './app.js' } );
+});
+
+// restart server if app.js changed
+gulp.task( 'server:restart', function() {
+    gulp.watch( [ './app.js' ], server.restart );
+});
+
+gulp.task('default', ['lint','server:start','server:restart']);
