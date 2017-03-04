3695a99c33bbb5331911d11ea6d0fe7be263bdd1
Christopher Hooper <hooper.christopher@gmail.com>
2017-03-03 23:22:02 -0800

    Add Stormpath

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var mongodb = require("mongodb");
+var path = require('path');
 var ObjectID = mongodb.ObjectID;
-
 var CONTACTS_COLLECTION = "contacts";
 
 var app = express();
@@ -11,6 +11,36 @@ app.use(bodyParser.json());
 // Create link to Angular build directory
 var distDir = __dirname + "/dist/";
 app.use(express.static(distDir));
+
+var stormpath = require('express-stormpath');
+
+app.set('trust proxy', true);
+app.use(function (req, res, next) {
+  console.log(new Date, req.method, req.url);
+  next();
+});
+console.log('Initializing Stormpath');
+
+app.use(stormpath.init(app, {
+  web: {
+    spa: {
+      enabled: true,
+      view: path.join(distDir, 'index.html')
+    },
+    me: {
+      // enabled: false,
+      expand: {
+        customData: true,
+        groups: true
+      }
+    }
+  }
+}));
+
+app.on('stormpath.ready', function () {
+  console.log('Stormpath Ready');
+});
+
 
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 var db;
@@ -33,18 +63,11 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
   });
 });
 
-// CONTACTS API ROUTES BELOW
-
 // Generic error handler used by all endpoints.
 function handleError(res, reason, message, code) {
   console.log("ERROR: " + reason);
   res.status(code || 500).json({"error": message});
 }
-
-/*  "/api/contacts"
- *    GET: finds all contacts
- *    POST: creates a new contact
- */
 
 app.get("/api/contacts", function(req, res) {
   db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
@@ -72,12 +95,6 @@ app.post("/api/contacts", function(req, res) {
     }
   });
 });
-
-/*  "/api/contacts/:id"
- *    GET: find contact by id
- *    PUT: update contact by id
- *    DELETE: deletes contact by id
- */
 
 app.get("/api/contacts/:id", function(req, res) {
   db.collection(CONTACTS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
@@ -112,3 +129,4 @@ app.delete("/api/contacts/:id", function(req, res) {
     }
   });
 });
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Stormpath, Account } from 'angular-stormpath';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  private user$: Observable<Account | boolean>;
+  private loggedIn$: Observable<boolean>;
+  private login: boolean;
+  private register: boolean;
+
+  constructor(public stormpath: Stormpath) {
+  }
+
+  ngOnInit() {
+    this.login = true;
+    this.register = false;
+    this.user$ = this.stormpath.user$;
+    this.loggedIn$ = this.user$.map(user => !!user);
+  }
+
+  showLogin() {
+    this.login = !(this.register = false);
+  }
+
+  showRegister() {
+    this.register = !(this.login = false);
+  }
+
+  logout() {
+    this.stormpath.logout();
+  }
 }
