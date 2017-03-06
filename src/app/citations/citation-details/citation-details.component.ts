7c0eb93c95cb6a3ee1ede93641ca2990879714c2
Christopher Hooper <hooper.christopher@gmail.com>
2017-03-06 13:56:46 -0800

    Move editor to citation-details component

--- src/app/citations/citation-details/citation-details.component.ts
+++ src/app/citations/citation-details/citation-details.component.ts
@@ -1,14 +1,24 @@
-import { Component, Input } from '@angular/core';
-import { Citation } from '../citation';
-import { CitationService } from '../citation.service';
-
+import {
+  Component, Input,
+  OnDestroy,
+  AfterViewInit,
+  EventEmitter,
+  Output
+} from '@angular/core';
+import {Citation} from '../citation';
+import {CitationService} from '../citation.service';
+declare var tinymce: any;
 @Component({
   selector: 'citation-details',
   templateUrl: './citation-details.component.html',
   styleUrls: ['./citation-details.component.css']
 })
 
-export class CitationDetailsComponent {
+export class CitationDetailsComponent implements AfterViewInit, OnDestroy {
+  @Input() elementId: String;
+
+  @Output() onEditorKeyup = new EventEmitter<any>();
+
   @Input()
   citation: Citation;
 
@@ -19,7 +29,25 @@ export class CitationDetailsComponent {
   @Input()
   deleteHandler: Function;
 
-  constructor (private citationService: CitationService) {}
+  constructor(private citationService: CitationService) {
+  }
+
+  editor;
+
+  ngAfterViewInit() {
+    tinymce.init({
+      selector: '#' + this.elementId,
+      plugins: ['link', 'paste', 'table'],
+      skin_url: 'assets/skins/lightgray',
+      setup: editor => {
+        this.editor = editor;
+        editor.on('keyup', () => {
+          const content = editor.getContent();
+          this.onEditorKeyup.emit(content);
+        });
+      },
+    });
+  }
 
   createCitation(citation: Citation) {
     this.citationService.createCitation(citation).then((newCitation: Citation) => {
@@ -38,4 +66,10 @@ export class CitationDetailsComponent {
       this.deleteHandler(deletedCitationId);
     });
   }
+
+  ngOnDestroy() {
+    tinymce.remove(this.editor);
+  }
+
 }
+
