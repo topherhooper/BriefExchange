06dd65c92584644457f91a7ef43fbe2fafa392bf
Christopher Hooper <hooper.christopher@gmail.com>
2017-03-05 16:58:00 -0800

    reduce stormpath parts to simple implementation

--- server.js
+++ server.js
@@ -3,8 +3,18 @@ var bodyParser = require("body-parser");
 var mongodb = require("mongodb");
 var ObjectID = mongodb.ObjectID;
 var CONTACTS_COLLECTION = "contacts";
+var stormpath = require('express-stormpath');
 
 var app = express();
+app.use(stormpath.init(app, {
+  // Optional configuration options.
+}));
+app.listen(3000);
+// Stormpath will let you know when it's ready to start authenticating users.
+app.on('stormpath.ready', function () {
+  console.log('Stormpath Ready!');
+});
+
 app.use(bodyParser.json());
 
 // Create link to Angular build directory
