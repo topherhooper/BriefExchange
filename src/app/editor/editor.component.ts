a4f8f362f8471a6c59ba9d8dcc8898681d9c8da4
Christopher Hooper <hooper.christopher@gmail.com>
2017-03-06 17:44:57 -0800

    Develop editor

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -22,6 +22,7 @@ export class EditorComponent implements AfterViewInit, OnDestroy {
   citations: Citation[]
   selectedCitation: Citation
   @Input() elementId: String;
+  @Input() client_id: string;
   @Output() onEditorKeyup = new EventEmitter<any>();
 
   editor;
@@ -41,15 +42,24 @@ export class EditorComponent implements AfterViewInit, OnDestroy {
 
   ngAfterViewInit() {
     tinymce.init({
-      selector: '#' + this.elementId,
+      formats: {smallcaps: {inline: 'span', 'classes': 'small-caps'}},
+      content_css: 'css/editor.css',
+      selector: 'textarea',
+      toolbar: 'undo redo | styleselect | bold italic | link image | smallcaps',
+      min_width: 700,
+      min_height: 70,
       plugins: ['link', 'paste', 'table'],
       skin_url: 'assets/skins/lightgray',
+      paste_enable_default_filters: false,
+      paste_filter_drop: false,
+      paste_retain_style_properties: "all",
+      paste_merge_formats: false,
       setup: editor => {
         this.editor = editor;
         editor.addButton('smallcaps', {
           title: 'Smallcaps',
           icon: 'forecolor',
-          onclick: function (evt) {
+          onclick: function () {
             editor.focus();
             editor.undoManager.beforeChange();//Preserve highlighted area for undo
             editor.formatter.toggle('smallcaps');
@@ -75,11 +85,13 @@ export class EditorComponent implements AfterViewInit, OnDestroy {
     tinymce.remove(this.editor);
   }
 
-  createNewCitation(citation_client) {
+  createNewCitation() {
     var citation: Citation = {
-      name: '',
-      citation_client: citation_client,
-      citation_fixed: ''
+      citation_client: tinymce.activeEditor.getContent({format: 'raw'}),
+      client_id: this.client_id,
+      citation_worker: '',
+      worker_id: '',
+      status: ''
     };
 
     // By default, a newly-created citation will have the selected state.
@@ -95,7 +107,8 @@ export class EditorComponent implements AfterViewInit, OnDestroy {
     var handler = (<any>window).StripeCheckout.configure({
       key: 'pk_live_wmonvJiL1mkU14vAZa2J8Xy8',
       locale: 'auto',
-      token: function (token: any) {}
+      token: function (token: any) {
+      }
     });
     handler.open({name: name, amount: amount});
 
